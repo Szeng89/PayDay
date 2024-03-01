03bfb1d51d19874e7e464b8cbfa91564563ddcab
Szeng89 <shijinz898@gmail.com>
2024-03-01 12:06:28 -0500

    Finished

--- src/test/java/rocks/zipcode/PayDayTest.java
+++ src/test/java/rocks/zipcode/PayDayTest.java
@@ -9,7 +9,18 @@ public class PayDayTest {
     @Test
     public void pay() {
         PayDay payDay1 = new PayDay();
+        PayDay payDay2 = new PayDay();
+        PayDay payDay3 = new PayDay();
+
         java.lang.String string1 = payDay1.pay("Kris", 21.5, 10.0, 0.05);
         assertEquals("Kris 215.00 10.75 204.25", string1);
+
+        java.lang.String string2 = payDay2.pay("Dolio", 23.0, 12.0, 0.05);
+        assertEquals("Kris 215.00 10.75 204.25", string1);
+
+        java.lang.String string3 = payDay3.pay("Karen", 22.0, 12.0, 0.05);
+        assertEquals("Kris 215.00 10.75 204.25", string1);
+
+
     }
 }
